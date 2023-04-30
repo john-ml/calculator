cff46c3130fc3c885209f24cff21f2fa8d21d001
John Li <johnli0135@gmail.com>
2023-04-30 19:39:27 -0400

    tidy; impl simple arithmetic calculator

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-To use:
-
-```
-rlwrap ocaml Unix.cma
-```
+To use: `python3 server.py` and open `127.0.0.1:5000` in browser.
 
 Depends on [tek](https://github.com/john-ml/tek).
--- server.py
+++ server.py
@@ -2,25 +2,37 @@ import flask as F
 from flask import Flask
 
 def base64image(path):
-    import base64
-    with open(path, 'rb') as f:
-        bytes = f.read()
-    return base64.b64encode(bytes).decode('utf-8')
+  import base64
+  with open(path, 'rb') as f:
+    bytes = f.read()
+  return base64.b64encode(bytes).decode('utf-8')
 
 def img(path):
-    return f'<img src="data:image/png;base64,{base64image(path)}"></img>'
+  return f'<img src="data:image/png;base64,{base64image(path)}"></img>'
 
 app = Flask(__name__)   
 
 @app.route('/', methods = ['GET'])
 def main():
-    return F.render_template('index.html')
+  return F.render_template('index.html')
 
 @app.route('/input', methods = ['POST'])
 def on_input():
-    print(F.request.data)
-    # return 'Your key is ' + request.data.decode('utf-8') + ' and here is a bunch more data'
-    return img('tmp/lfman.png')
+  from html import unescape
+  s = unescape(F.request.data.decode('utf-8'))
+  if s.endswith('<br>'): s = s[:-len('<br>')]
+  print('Received input:', s)
+  try:
+    def go(stack, cmds):
+      print(stack, cmds)
+      match stack, cmds:
+        case [h, *_], []: return h
+        case [v, w, *stack], ['+', *cmds]: return go([v + w, *stack], cmds)
+        case [v, w, *stack], ['*', *cmds]: return go([v * w, *stack], cmds)
+        case stack, [c, *cmds]: return go([float(c), *stack], cmds)
+    return str(go([], s.split()))
+  except:
+    return img('lfman.png')
 
 if __name__ == '__main__':
-   app.run()
+  app.run()
