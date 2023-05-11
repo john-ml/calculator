a2b00540c74b040c3640b19ffc1b6113b198d025
John Li <johnli0135@gmail.com>
2023-05-11 14:03:04 -0400

    more lark tests

--- lark_tests.py
+++ lark_tests.py
@@ -1,5 +1,6 @@
 from lark import Lark, Transformer, Tree
 from ast import literal_eval
+from lark.visitors import CollapseAmbiguities
 
 # ---------- The running example from the tutorial ----------
 
@@ -40,6 +41,31 @@ print(json_parser.parse(json))
 
 # ---------- Trying out a highly ambiguous grammar ----------
 
-json_parser = Lark(open('amb.lark').read(), start='value', tree_class=inline_transformer)
-json = '{"key": ["s", "\\n", 3.14, true, null]}'
-print(json_parser.parse(json))
+term_parser = Lark(open('amb.lark').read(), start='term')
+print(term_parser.parse('1 + 1 + 1').pretty())
+print(term_parser.parse('(1 + 1) + 1').pretty())
+print(term_parser.parse('1 * 1 + 1').pretty())
+print(term_parser.parse('1 + 1 * 1').pretty())
+print('-'*10)
+term_parser = Lark(open('amb.lark').read(), start='term', ambiguity='explicit')
+ts = term_parser.parse('1 * 1 + 1 + 1')
+for t in CollapseAmbiguities().transform(ts): print(t.pretty())
+print(ts)
+print(ts.pretty())
+
+# def evaluate(op, xs):
+#   match op, xs:
+#     case 'add', [x, y]: return x + y
+#     case 'sub', [x, y]: return x - y
+#     case 'mul', [x, y]: return x * y
+#     case 'div', [x, y]: return x / y
+#     case 'div', [x, y]: return x / y
+#     case 'string', _: return 0
+#     case 'true', _: return 1
+#     case 'false', _: return 0
+#     case 'number', [x]: return float(x)
+#     case _, _: assert False
+
+# term_parser = Lark(open('amb.lark').read(), start='term', tree_class=evaluate)
+# term = '1 + 1 + 1'
+# print(term_parser.parse(term))
