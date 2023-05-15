3035be1a13cffb76b48e07f27f723e08b290a4a3
John Li <johnli0135@gmail.com>
2023-05-15 17:21:11 -0400

    doc comments

--- poset.py
+++ poset.py
@@ -1,7 +1,18 @@
 import networkx as N
 
-# A poset with top and bottom elements 'bot' and 'top'
 class Poset:
+  '''
+  A poset with support for top and bottom elements.
+  Maintains a DAG of SCC-equivalence-classes, called the 'skeleton', whose
+  transitive closure is the poset. For example, if have the poset
+    a <= b
+    b <= a
+    a <= c <= d
+    a <= d
+  this class maintains the dag
+    {a, b} <= {c} <= {d}
+  where a and b are identified and the transitive inequality a <= d is deleted.
+  '''
   def __init__(self):
     self.bots = set()
     self.tops = set()
@@ -20,6 +31,7 @@ class Poset:
     self.partition = []
     self.skeleton = N.DiGraph()
   def make_closure(self):
+    '''Recompute the transitive closure.'''
     if self.good_closure: return
     self.closure = N.DiGraph(self.graph)
     self.closure.add_edges_from([('bot', v) for v in self.closure.nodes])
@@ -29,6 +41,7 @@ class Poset:
     self.closure = N.transitive_closure(self.closure, reflexive=True)
     self.good_closure = True
   def make_skeleton(self):
+    '''Recompute the skeleton.'''
     if self.good_skeleton: return
     self.make_closure()
     partition = tuple(N.strongly_connected_components(self.closure))
@@ -39,35 +52,50 @@ class Poset:
     self.skeleton = g
     self.good_skeleton = True
   def touch_closure(self):
+    '''Mark transitive closure for recomputation.'''
     self.good_closure = False
     self.good_skeleton = False
   def touch_skeleton(self):
+    '''Mark skeleton for recomputation.'''
     self.good_skeleton = False
   def add_bot(self, b):
+    '''Add b as a bottom element.'''
     self.touch_closure()
     self.bots.add(b)
     return self
   def add_top(self, t):
+    '''Add t as a top element.'''
     self.touch_closure()
     self.tops.add(t)
     return self
   def add(self, x):
+    '''Add x as an element of the poset.'''
     self.touch_closure()
     self.graph.add_node(x)
     return self
   def add_le(self, x, y):
+    '''Add the relation x <= y.'''
     self.touch_closure()
     self.graph.add_edge(x, y)
     return self
   def le(self, x, y):
+    '''Check whether x <= y in the poset.'''
     self.make_closure()
     return self.closure.has_edge(x, y)
   def canon(self, x):
+    '''
+    Return the SCC-equivalence-class containing x, represented as a frozenset()
+    of vertices.
+    '''
     self.make_closure()
     return self.partition[x]
-  def succs(self, x):
+  def succs(self, eclass):
+    '''
+    Given an SCC-equivalence-class, represented as a frozenset() of vertices,
+    return its successors in the skeleton.
+    '''
     self.make_closure()
-    return self.skeleton.successors(x)
+    return self.skeleton.successors(eclass)
 
 if __name__ == '__main__':
   g = N.DiGraph([(0, 1), (1, 0), (2, 2), (0, 3), (3, 4)])
