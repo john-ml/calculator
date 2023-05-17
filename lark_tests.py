2cb57b5a97fcdce03a5921b19c33753bf57db2e8
John Li <johnli0135@gmail.com>
2023-05-16 22:44:56 -0400

    first pass at writing a forest transformer

--- lark_tests.py
+++ lark_tests.py
@@ -269,8 +269,15 @@ print(ts.pretty())
 term_parser = Lark(r'''
       ?term : term_5_8
       | term_9_12
+      | term_13_16
+      | term_17_16
+      | term_19_15
+      | term_22_25
+      | term_26_24
 
-      ?term_5_8 : term_1_4 "+" term_5_8 -> c_plus
+      c_plus : term_1_4 "+" term_5_8
+
+      ?term_5_8 : c_plus
       | term_1_4
 
       ?term_1_4 : term_0_0 "*" term_1_4 -> c_times
@@ -282,12 +289,38 @@ term_parser = Lark(r'''
       ?term_9_12 : term_5_8 "->" term_9_12 -> c_pow
       | term_0_0
 
-      ?atom : name -> var
+      ?term_13_16 : "forall " name "." term_29_16 -> c_forall
+      | term_0_0
+
+      ?term_29_16 : term_13_16
+      | term_17_16
+      | term_1_4
+      | term_19_15
+
+      ?term_17_16 : "exists " name "." term_29_16 -> c_exists
+      | term_0_0
+
+      ?term_19_15 : term_0_0 "=" term_0_0 -> c_eq
+      | term_0_0
+
+      ?term_22_25 : "\\" name "." term_29_25 -> c_lam
+      | term_0_0
+
+      ?term_29_25 : term_22_25
+      | term_26_24
+
+      ?term_26_24 : c_app
+      | term_0_0
+
+      c_app : term_26_24 " " term_0_0
+
+      ?atom : name
       | ESCAPED_STRING -> string
       | SIGNED_NUMBER -> number
-      | "(" term ")" -> parens
+      | parens
 
       name : CNAME
+      parens : "(" term ")"
 
       %import common.CNAME
       %import common.ESCAPED_STRING
@@ -297,47 +330,77 @@ term_parser = Lark(r'''
 ''', start='term', ambiguity='forest')
 
 def graph_of(f):
+  # Map each vertex's id to a [str dump of data at that vertex, is vertex an OR node?, list of child ids]
+  # In Lark the OR nodes are called SymbolNodes and the AND nodes are called PackedNodes.
   graph = {}
-  def go(f):
+  def go(or_node, f):
     nonlocal graph
     if id(f) in graph: return
-    graph[id(f)] = [str(f), []] if not hasattr(f, 'children') else [str(f.s) + ' ' + str(f), list(map(id, f.children))]
+    graph[id(f)] = [str(f) + ' ' + str(type(f)), or_node, []] if not hasattr(f, 'children') else [str(f.s) + ' ' + str(type(f.s)) + ' ' + str(f) + ' ' + str(type(f)) + ' ' + str(f.is_intermediate if 'SymbolNode' in str(type(f)) else ''), or_node, list(map(id, f.children))]
     if hasattr(f, 'children'):
       for c in f.children:
-        go(c)
-  go(f)
+        go(not or_node, c)
+  go(True, f)
   return graph
 
 def viz(graph):
   entries = []
-  for v, (pretty, _) in graph.items():
-    entries.append(f'v{v} [label={repr(pretty)}]')
-  for v, (_, children) in graph.items():
+  mk_color = lambda or_node: 'blue' if or_node else 'black'
+  for v, (pretty, or_node, _) in graph.items():
+    entries.append(f'v{v} [label={repr(pretty)},color={mk_color(or_node)}]')
+  for v, (_, or_node, children) in graph.items():
     for i, c in enumerate(children):
-      entries.append(f'v{v} -> v{c} [label={i}]')
+      entries.append(f'v{v} -> v{c} [label={i},color={mk_color(or_node)}]')
   entries = ';\n'.join(entries)
   return f'digraph {{ {entries} }}'
 
+# Manually implement filtering of ?nonterm
 def contract(graph):
   visited = set()
   def go(parent, v):
     nonlocal visited
     if v in visited: return
-    s, children = graph[v]
-    for c in children:
-      go(v, c)
-    chop = [(0,0),(1,4),(9,12),(5,8)]
-    if (any(f'term_{i}_{j}' in s for i,j in chop)) and parent is not None and len(graph[parent][1]) == 1:
-      graph[parent][1] = graph[v][1]
+    s, or_node, children = graph[v]
+    for i, c in enumerate(children):
+      go((v, i), c)
+    chop = [
+      "'term'",
+      'atom',
+      'term_1_4',
+      'term_0_0',
+      'term_9_12',
+      'term_5_8',
+      'term_13_16',
+      'term_29_16',
+      'term_17_16',
+      'term_19_15',
+      'term_22_25',
+      'term_29_25',
+      'term_26_24',
+    ]
+    if parent is not None and (any(t in s for t in chop)) and or_node and len(children) == 1 and len(graph[children[0]][2]) == 1:
+      p, i = parent
+      graph[p][2][i] = graph[children[0]][2][0]
   go(None, id(f))
+
+# Coalesce OR-node children that are the same to increase sharing
+def coalesce(graph):
+  while True:
+    done = True
+    for v in tuple(v for v in graph):
+      _, or_node, children = graph[v]
+      deduplicated = set(tuple(graph[w][2]) for w in children)
+      if or_node and len(children) > 1 and len(deduplicated) == 1:
+        graph[v][2] = [children[0]]
+        done = False
+    if done: return
 
 def gc(graph, roots):
   extracted = {}
   def go(v):
-    nonlocal extracted
     if v in extracted: return
     extracted[v] = graph[v]
-    _, children = graph[v]
+    _, _, children = graph[v]
     for c in children:
       go(c)
   for root in roots:
@@ -347,10 +410,78 @@ def gc(graph, roots):
   for v in extracted:
     graph[v] = extracted[v]
 
+def proper_contract(f):
+  from lark.parsers.earley_forest import ForestTransformer, SymbolNode, PackedNode
+  from lark.grammar import NonTerminal
+  from lark.lexer import Token
+  packed_node_is_singleton = lambda node: (node.left is None, node.right is None) in {(True, False), (False, True)}
+  class T(ForestTransformer):
+    def transform_symbol_node(self, node, data):
+      contractible = lambda s: s.name.startswith('term') or s.name in {'atom'}
+      can_contract = (
+        contractible(node.s)
+        and len(data) == 1
+        and packed_node_is_singleton(data[0])
+      )
+      if can_contract:
+        if data[0].left is None: return data[0].right
+        else: return data[0].left
+      else:
+        s = SymbolNode(node.s, node.start, node.end)
+        # Probably sinful, but I don't see another way to get at the children of these nodes
+        s._children = set(data)
+        return s
+    def transform_intermediate_node(self, node, data):
+      can_contract = (
+        False and
+        len(data) == 1
+        and packed_node_is_singleton(data[0])
+      )
+      if can_contract:
+        if data[0].left is None: return data[0].right
+        else: return data[0].left
+      else:
+        s = SymbolNode(node.s, node.start, node.end)
+        # Probably sinful, but I don't see another way to get at the children of these nodes
+        s._children = set(data)
+        return s
+    def transform_packed_node(self, node, data):
+      match data:
+        case []: return PackedNode(node.parent, node.s, node.rule, node.start, None, None)
+        case [p]: return PackedNode(node.parent, node.s, node.rule, node.start, None, p)
+        case [p, q]: 
+          # Ignore string literals found in any intermediate node
+          eligible = lambda s: type(s) is tuple or (type(s) is NonTerminal and s.name not in {'name'})
+          if eligible(node.parent.s): 
+            print('node.parent.s\t', node.parent.s)
+            print('node.s\t\t', node.s)
+            print('p, type(p)\t', p, type(p))
+            print('q, tyqe(q)\t', q, type(q))
+            print()
+          default = lambda: PackedNode(node.parent, node.s, node.rule, node.start, p, q)
+          if eligible(node.parent.s):
+            match type(p) is Token, type(q) is Token:
+              case True, True:
+                print('wat', node, type(node), type(p), type(q), p, q)
+                return default()
+              case True, _ if len(q.children) == 1: return q.children[0]
+              case _, True if len(p.children) == 1: return p.children[0]
+              case _: return default()
+          else: return default()
+        case _: assert False
+    def transform_token_node(self, node):
+      return node
+  return T().transform(f)
+
 import pyperclip
 
-f = term_parser.parse('(((x)))')
+# f = term_parser.parse('(((x)))')
+f = term_parser.parse('(x y)')
+f = proper_contract(f)
 g = graph_of(f)
-contract(g)
+# contract(g)
+# coalesce(g)
+# contract(g)
 gc(g, [id(f)])
 pyperclip.copy(viz(g))
+print('Graph copied to clipboard')
