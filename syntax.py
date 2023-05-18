a59b60ef600c867334c8ad9930354f2f205c2dee
John Li <johnli0135@gmail.com>
2023-05-18 12:23:29 -0400

    split long LC example across multiple lines

--- syntax.py
+++ syntax.py
@@ -1107,7 +1107,21 @@ if __name__ == '__main__':
     expect((1, 0), (count, dups))
 
   # Long input that can't be parsed efficiently without good grammar generation
-  expect_unambiguous(r'(\Y.\iszero.\add.\z.\s.\cons.\fst.\snd.(\pred.(\tri. tri (s (s (s z)))) (Y (\tri.\n.iszero n z (add n (tri (pred n)))))) (\n.fst (n (\p. cons (snd p) (s (snd p))) (cons z z)))) (\f.(\x.(f (x x))) (\x.(f (x x)))) (\n.n (\n.\t.\f.f) (\t.\f.t)) (\m.\n.\s.\z.m s (n s z)) (\s.\z.z) (\n.\s.\z.s (n s z)) (\x.\y.\f.f x y) (\p.p (\x.\y.x)) (\p.p (\x.\y.y))')
+  expect_unambiguous(' '.join(r'''
+    (\Y.\iszero.\add.\z.\s.\cons.\fst.\snd.
+      (\pred.
+        (\tri. tri (s (s (s z))))
+        (Y (\tri.\n.iszero n z (add n (tri (pred n))))))
+      (\n.fst (n (\p. cons (snd p) (s (snd p))) (cons z z))))
+    (\f.(\x.(f (x x))) (\x.(f (x x))))
+    (\n.n (\n.\t.\f.f) (\t.\f.t))
+    (\m.\n.\s.\z.m s (n s z))
+    (\s.\z.z)
+    (\n.\s.\z.s (n s z))
+    (\x.\y.\f.f x y)
+    (\p.p (\x.\y.x))
+    (\p.p (\x.\y.y))
+  '''.split()))
 
   # Manually contracting SPPF avoids exponential blowups in the number of parse trees
   expect_unambiguous('(a) (b) (c) (d) (e) (f) (g) (h) (i) (j) (k) (l)')
